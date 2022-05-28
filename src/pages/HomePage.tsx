a991327b68cc82522836a680b464e6783d3f4076
PARK JI EUN <pjieuniv20@ewhain.net>
2022-05-28 20:19:32 +0900

    initial

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const HomePage = () => {
+  return (
+  <div>
+    <div className='py-6 flex items-center justify-center'>
+      <img src="https://images.unsplash.com/photo-1517495306984-f84210f9daa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+      alt=""
+      className='rounded-full w-52 h-52'/>
+      <div className=''>24</div>
+  </div>
+  </div>
+  
+  );
+};
